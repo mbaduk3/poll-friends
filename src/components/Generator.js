8b674c3cde02e40670f72a9c8928114b02012418
Maxim Baduk <mb2474@cornell.edu>
2019-12-26 20:43:22 -0500

    generator

---
+++ src/components/Generator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css'
+
+class Generator extends React.Component {
+
+    constructor() {
+        super();
+        this.state={};
+    }
+
+    render() {
+        return (
+            <div class="generator-div">
+                Generator here
+            </div>
+        )
+    }
+}
+
+export default Generator
