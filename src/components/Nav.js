6126800d55f739590724c79725d665c3947abbcb
Maxim Baduk <mb2474@cornell.edu>
2019-12-26 20:28:33 -0500

    nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+    return (
+        <nav>
+            <a>PollFriends</a>
+            <button class="state-button">Save</button>
+            <button class="state-button">Send</button>
+        </nav>
+    )
+}
+
+export default Nav
